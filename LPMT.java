8faa2a31abc9b7ccfca2da6cf5d23c3a1fe2893e
kipngeno koech <kkipngenokoech22@gmail.com>
2024-08-11 15:33:04 +0200

    building the future today

--- LPMT.java
+++ LPMT.java
@@ -114,7 +114,6 @@ public class LPMT {
                         System.out.println(Design.padMessage(Design.formatMessage("Login successful, welcome "+username, Design.GREEN_COLOR),50));
                         System.out.print(Design.formatMessage("Redirecting to dashboard", Design.YELLOW_COLOR));
                         printLoadingDots(5);
-                        System.out.println(role);
                             if (role.equals("admin")){
                                 admin.callAdminMenu();
                             } else {
